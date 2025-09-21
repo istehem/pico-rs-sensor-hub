d67dea82ae6acd6cb59ccc570f08c47f333ad8eb
Ingemarsson Oskar <oskar@oskar.me>
2025-09-21 15:27:34 +0200

    started refactoring tests

--- tests/test_dice.rs
+++ tests/test_dice.rs
@@ -1,25 +1,32 @@
 #[cfg(test)]
 mod tests {
+    use rstest::fixture;
     use rstest::rstest;
-    use tracing::info;
 
     use core::convert::Infallible;
     use embedded_graphics::{pixelcolor::Rgb888, prelude::*};
     use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
     use pico_display::dice;
 
-    #[rstest]
-    #[test_log::test]
-    fn test_hello_word() -> () {
-        info!("Hello World!");
+    type Display = SimulatorDisplay<Rgb888>;
+
+    #[fixture]
+    #[once]
+    fn shared_display() -> Display {
+        SimulatorDisplay::new(Size::new(255, 255))
+    }
+
+    #[fixture]
+    fn shared_display_clone(#[from(shared_display)] display: &Display) -> Display {
+        display.clone()
     }
 
     /*
     #[rstest]
     #[test_log::test]
-    fn test_draw_dice_one() -> Result<(), Infallible> {
-        let mut display: SimulatorDisplay<Rgb888> = SimulatorDisplay::new(Size::new(255, 255));
-
+    fn test_draw_dice_one(
+        #[from(shared_display_clone)] mut display: Display,
+    ) -> Result<(), Infallible> {
         dice::draw_one(&mut display.translated(Point::new(8, 8)), 255 - 8 - 8)?;
 
         let output_settings = OutputSettingsBuilder::new().scale(1).build();
@@ -31,9 +38,9 @@ mod tests {
 
     #[rstest]
     #[test_log::test]
-    fn test_draw_dice_two() -> Result<(), Infallible> {
-        let mut display: SimulatorDisplay<Rgb888> = SimulatorDisplay::new(Size::new(255, 255));
-
+    fn test_draw_dice_two(
+        #[from(shared_display_clone)] mut display: Display,
+    ) -> Result<(), Infallible> {
         dice::draw_two(&mut display.translated(Point::new(8, 8)), 255 - 8 - 8)?;
 
         let output_settings = OutputSettingsBuilder::new().scale(1).build();
