f439ecfd20eb7deaaa0d4685eb89855fa7b99f89
Ingemarsson Oskar <oskar@oskar.me>
2025-09-21 16:53:44 +0200

    started refactoring drawing dice

--- crates/pico-display/src/dice.rs
+++ crates/pico-display/src/dice.rs
@@ -6,21 +6,40 @@ use embedded_graphics::{
 
 use num_traits::float::FloatCore;
 
+struct Pip {
+    size: u32,
+    style: PrimitiveStyle<BinaryColor>,
+}
+
+impl Pip {
+    fn new(size: u32) -> Self {
+        let style = PrimitiveStyle::with_fill(BinaryColor::On);
+        Self { size, style }
+    }
+
+    fn draw<T>(&self, target: &mut T, x: i32, y: i32) -> Result<(), <T as DrawTarget>::Error>
+    where
+        T: DrawTarget<Color = BinaryColor>,
+    {
+        Circle::new(Point::new(x, y), self.size)
+            .into_styled(self.style)
+            .draw(target)
+    }
+}
+
 pub fn draw_one<T>(target: &mut T, side_length: u32) -> Result<(), T::Error>
 where
     T: DrawTarget<Color = BinaryColor>,
 {
     let stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
-    let stroke_off_fill_on = PrimitiveStyle::with_fill(BinaryColor::On);
+
+    let pip_size = percent_of_to_nearest_odd(side_length, 13);
+    let pip = Pip::new(pip_size);
 
     let middle = (side_length - 1) / 2;
-    let pip_size = percent_of_to_nearest_odd(side_length, 13);
-
     let pip_starts_at = (middle - (pip_size - 1) / 2) as i32;
 
-    Circle::new(Point::new(pip_starts_at, pip_starts_at), pip_size)
-        .into_styled(stroke_off_fill_on)
-        .draw(target)?;
+    pip.draw(target, pip_starts_at, pip_starts_at)?;
 
     RoundedRectangle::new(
         Rectangle::new(Point::new(0, 0), Size::new(side_length, side_length)),
@@ -36,30 +55,19 @@ where
     T: DrawTarget<Color = BinaryColor>,
 {
     let stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
-    let stroke_off_fill_on = PrimitiveStyle::with_fill(BinaryColor::On);
+
+    let pip_size = percent_of_to_nearest_odd(side_length, 13);
+    let pip = Pip::new(pip_size);
 
     let middle = (side_length - 1) / 2;
-    let pip_size = percent_of_to_nearest_odd(side_length, 13);
+    let middle_offset = (middle - 1) / 2;
+    let first_pip_starts_at = (middle - middle_offset - (pip_size - 1) / 2) as i32;
 
-    let middle_off_set = (middle - 1) / 2;
+    pip.draw(target, first_pip_starts_at, first_pip_starts_at)?;
 
-    let first_pip_starts_at = (middle - middle_off_set - (pip_size - 1) / 2) as i32;
+    let second_pip_starts_at = (middle + middle_offset - (pip_size - 1) / 2) as i32;
 
-    Circle::new(
-        Point::new(first_pip_starts_at, first_pip_starts_at),
-        pip_size,
-    )
-    .into_styled(stroke_off_fill_on)
-    .draw(target)?;
-
-    let second_pip_starts_at = (middle + middle_off_set - (pip_size - 1) / 2) as i32;
-
-    Circle::new(
-        Point::new(second_pip_starts_at, second_pip_starts_at),
-        pip_size,
-    )
-    .into_styled(stroke_off_fill_on)
-    .draw(target)?;
+    pip.draw(target, second_pip_starts_at, second_pip_starts_at)?;
 
     RoundedRectangle::new(
         Rectangle::new(Point::new(0, 0), Size::new(side_length, side_length)),
