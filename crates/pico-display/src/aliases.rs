d57cd242d6b550ce93454282a9eab7197f1df605
Ingemarsson Oskar <oskar@oskar.me>
2025-09-27 12:26:28 +0200

    make trait aliasas dependent

--- crates/pico-display/src/aliases.rs
+++ crates/pico-display/src/aliases.rs
@@ -2,9 +2,6 @@ use embedded_graphics::{geometry::OriginDimensions, pixelcolor::BinaryColor, pre
 use trait_set::trait_set;
 
 trait_set! {
-    pub trait Display = DrawTarget<Color = BinaryColor> + OriginDimensions;
-}
-
-trait_set! {
     pub trait DrawTarget = prelude::DrawTarget<Color = BinaryColor>;
+    pub trait Display = DrawTarget + OriginDimensions;
 }
