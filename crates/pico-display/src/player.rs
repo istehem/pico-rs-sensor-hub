4ec1fa7ae90ef94c4777e6f3e7893b9cd892c676
Ingemarsson Oskar <oskar@oskar.me>
2025-10-02 00:37:24 +0200

    don't lose the new seed when dice were rolled

--- crates/pico-display/src/player.rs
+++ crates/pico-display/src/player.rs
@@ -4,47 +4,47 @@ use rand::{Rng, SeedableRng};
 use crate::aliases::Display;
 use crate::dice;
 
-pub fn roll_die<T>(target: &mut T, seed: u64) -> Result<(), T::Error>
+pub fn roll_die<T>(target: &mut T, seed: u64) -> Result<SmallRng, T::Error>
 where
     T: Display,
 {
     let mut small_rng = SmallRng::seed_from_u64(seed);
     let face_value = || small_rng.random();
-    dice::draw_dice(target, 1, face_value)
+    dice::draw_dice(target, 1, face_value).map(|_| small_rng)
 }
 
-pub fn roll_two_dice<T>(target: &mut T, seed: u64) -> Result<(), T::Error>
+pub fn roll_two_dice<T>(target: &mut T, seed: u64) -> Result<SmallRng, T::Error>
 where
     T: Display,
 {
     let mut small_rng = SmallRng::seed_from_u64(seed);
     let face_value = || small_rng.random();
-    dice::draw_dice(target, 2, face_value)
+    dice::draw_dice(target, 2, face_value).map(|_| small_rng)
 }
 
-pub fn roll_three_dice<T>(target: &mut T, seed: u64) -> Result<(), T::Error>
+pub fn roll_three_dice<T>(target: &mut T, seed: u64) -> Result<SmallRng, T::Error>
 where
     T: Display,
 {
     let mut small_rng = SmallRng::seed_from_u64(seed);
     let face_value = || small_rng.random();
-    dice::draw_dice(target, 3, face_value)
+    dice::draw_dice(target, 3, face_value).map(|_| small_rng)
 }
 
-pub fn roll_four_dice<T>(target: &mut T, seed: u64) -> Result<(), T::Error>
+pub fn roll_four_dice<T>(target: &mut T, seed: u64) -> Result<SmallRng, T::Error>
 where
     T: Display,
 {
     let mut small_rng = SmallRng::seed_from_u64(seed);
     let face_value = || small_rng.random();
-    dice::draw_dice(target, 4, face_value)
+    dice::draw_dice(target, 4, face_value).map(|_| small_rng)
 }
 
-pub fn roll_five_dice<T>(target: &mut T, seed: u64) -> Result<(), T::Error>
+pub fn roll_five_dice<T>(target: &mut T, seed: u64) -> Result<SmallRng, T::Error>
 where
     T: Display,
 {
     let mut small_rng = SmallRng::seed_from_u64(seed);
     let face_value = || small_rng.random();
-    dice::draw_dice(target, 5, face_value)
+    dice::draw_dice(target, 5, face_value).map(|_| small_rng)
 }
