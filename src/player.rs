f307fbf71ccc608fdf9b6a881c85bcdffb52b54f
Ingemarsson Oskar <oskar@oskar.me>
2025-11-09 15:38:05 +0100

    organize imports

--- src/player.rs
+++ src/player.rs
@@ -1,10 +1,12 @@
-use crate::DrawError;
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 use defmt::info;
 use embedded_graphics::pixelcolor::BinaryColor;
+
 use game_logic::two_four_eighteen::{Game, NumberOfDice};
 use pico_display::aliases::Display as DisplayTrait;
+
+use crate::DrawError;
 
 #[derive(PartialEq)]
 pub enum GameResult {
