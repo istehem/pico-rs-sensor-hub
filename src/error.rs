b8df08830bc1eca202029347640b0534b4c772c4
Ingemarsson Oskar <oskar@oskar.me>
2025-11-12 22:44:32 +0100

    better error handling

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,9 @@
-use core::convert::Infallible;
+use core::{convert::Infallible, fmt};
+use u8g2_fonts::Error as U8g2Error;
 
 #[derive(Debug)]
 pub enum DrawError<DisplayError> {
-    FontError(u8g2_fonts::Error<DisplayError>),
+    FontError(U8g2Error<DisplayError>),
     DisplayError(DisplayError),
 }
 
@@ -12,31 +13,40 @@ impl<DisplayError> From<DisplayError> for DrawError<DisplayError> {
     }
 }
 
-impl<DisplayError> From<u8g2_fonts::Error<DisplayError>> for DrawError<DisplayError> {
-    fn from(e: u8g2_fonts::Error<DisplayError>) -> Self {
+impl<DisplayError> From<U8g2Error<DisplayError>> for DrawError<DisplayError> {
+    fn from(e: U8g2Error<DisplayError>) -> Self {
         DrawError::FontError(e)
     }
 }
 
-#[derive(Debug)]
 pub enum FontError {
-    Infallible,
-    BackgroundColorNotSupported,
-    GlyphNotFound,
+    Infallible(Infallible),
+    BackgroundColorNotSupported(U8g2Error<Infallible>),
+    GlyphNotFound(U8g2Error<Infallible>),
 }
 
 impl From<Infallible> for FontError {
-    fn from(_: Infallible) -> Self {
-        FontError::Infallible
+    fn from(e: Infallible) -> Self {
+        FontError::Infallible(e)
     }
 }
 
-impl From<u8g2_fonts::Error<Infallible>> for FontError {
-    fn from(e: u8g2_fonts::Error<Infallible>) -> Self {
+impl fmt::Debug for FontError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Infallible(err) => err.fmt(f),
+            Self::GlyphNotFound(err) => err.fmt(f),
+            Self::BackgroundColorNotSupported(err) => err.fmt(f),
+        }
+    }
+}
+
+impl From<U8g2Error<Infallible>> for FontError {
+    fn from(e: U8g2Error<Infallible>) -> Self {
         match e {
-            u8g2_fonts::Error::DisplayError(_) => Self::Infallible,
-            u8g2_fonts::Error::BackgroundColorNotSupported => Self::BackgroundColorNotSupported,
-            u8g2_fonts::Error::GlyphNotFound(_) => Self::GlyphNotFound,
+            U8g2Error::DisplayError(e) => Self::Infallible(e),
+            err @ U8g2Error::BackgroundColorNotSupported => Self::BackgroundColorNotSupported(err),
+            err @ U8g2Error::GlyphNotFound(_) => Self::GlyphNotFound(err),
         }
     }
 }
